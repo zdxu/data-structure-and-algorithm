91eaabd2c2340130874095d0b2291bab362c129d
zhaodexu <zhdxu@landhightech.com>
2019-09-22 23:54:01 +0800

    shell sort

---
+++ sort-algorithm/shell_sort.py
@@ -0,0 +1,30 @@
+# -*- coding=utf-8 -*-
+
+
+from insertion_sort import insert_sort_method
+
+
+def shell_sort_method(arr: list, step):
+    if step == 1:
+        return insert_sort_method(arr)
+
+    for i in range(step):
+        j = i + step
+        temp = arr[j]
+        while j < len(arr):
+            k = j - step
+            while k > 0:
+                if arr[k] > arr[j]:
+                    temp = arr[j]
+                    arr[j] = arr[k]
+                    arr[k] = temp
+                    k = k - step
+                else:
+                    break
+            j = j + step
+    return shell_sort_method(arr, step//2)
+
+
+if __name__ == "__main__":
+    arr = [1, 4, 10, 6, 7, 5, 4, 3, 2, 11, 15, 19, 13]
+    print(shell_sort_method(arr, len(arr)//2))
